4fd542d2aa8460850dcb64d5b48b63a36f88911f
Gavin <gavin.n.lobo@gmail.com>
2024-02-22 16:53:48 -0500

    big changes

--- include/gul/VirtualFileSystem.h
+++ include/gul/VirtualFileSystem.h
@@ -4,7 +4,6 @@
 #include <variant>
 #include <filesystem>
 #include <map>
-#include<fstream>
 #include <iostream>
 #include <vector>
 
@@ -134,44 +133,6 @@ struct VFS
 
     }
 
-    template<typename callable_t>
-    void for_each3(vfs_path_type const & p, callable_t && CC) const
-    {
-        auto it = desc.find(p);
-        while( it != desc.end())
-        {
-            if(std::holds_alternative<Mount>(it->second))
-            {
-                // this is a mount point, so loop through all the files
-                // on the host
-                auto & M = std::get<Mount>(it->second);
-                for(auto & A : std::filesystem::directory_iterator(M.host))
-                {
-                    CC(A.path().lexically_relative(M.host));
-                }
-
-                // But there might also be another mount point
-                // at  p / mnt
-                //
-            }
-            else if(std::holds_alternative<Directory>(it->second))
-            {
-                (void)is_base_of;
-                while(it != desc.end())
-                {
-                    auto name = it->first.lexically_relative(p);
-                    auto s = std::distance(name.begin(), name.end());
-                    if(s == 1 && name != ".")
-                        CC(name);
-                        //std::cout << name << "  " << s << std::endl;
-                    ++it;
-                }
-                return;
-            }
-            ++it;
-        }
-    }
-
     /**
      * @brief exists
      * @param pf
@@ -184,50 +145,18 @@ struct VFS
         assert(pf.is_absolute());
 
         auto it = desc.find(pf);
-
         // exists in the Virtual files
         if(it != desc.end())
         {
             return true;
         }
 
-        // pf doesn't have an explicit location in the file descriptors
-        // map
-        // so that means it is likely in some folder within a mounted
-        // filesystem
-        //
-        // So work backwards from the filename until we find
-        // a mount point:
-        // for example:
-        // Given pf = /path/to/some/file.txt
-        //
-        // and a mount point "/path -> /home/bob" exists
-        // then search if /home/bob/to/some/file.txt exists
-        // in the host file system and
-
-        // then left == path/to/some/file.txt
-        vfs_path_type left = pf.relative_path();
-        // right is empty
-        vfs_path_type right;
-
-        while(!left.empty())
+        auto [mnt, stem] = splitMount(pf);
+        if(!mnt.empty())
         {
-            // if 'left' is a mount point to the host
-            // then check if right exists the filesystem
-            if(is_mount(vfs_path_type("/") / left))
-            {
-                return std::filesystem::exists(std::get<Mount>(desc.at(vfs_path_type("/") / left)).host / right);
-            }
-
-            // remove the filename from 'left' and
-            // append it to the front of 'right'
-
-            // left = path/to/some
-            // right = file.txt
-            right = right.empty() ? left.filename() : (left.filename() / right);
-            left = left.parent_path();
+            auto & M = std::get<Mount>(desc.at(mnt));
+            return std::filesystem::exists( M.host / stem);
         }
-
         return false;
     }
 
@@ -263,7 +192,6 @@ struct VFS
         return {};
     }
 
-
     bool is_directory(vfs_path_type const & pf) const
     {
         assert(pf.is_absolute());
@@ -295,7 +223,6 @@ struct VFS
         {
             return std::holds_alternative<File>(it->second);
         }
-
         auto [mnt, stem] = splitMount(pf);
         if(!mnt.empty())
         {
@@ -304,6 +231,30 @@ struct VFS
         }
         return false;
     }
+
+    uintmax_t file_size(vfs_path_type const & pf) const
+    {
+        assert(pf.is_absolute());
+
+        auto it = desc.find(pf);
+        // exists in the Virtual files
+        if(it != desc.end())
+        {
+            if(std::holds_alternative<File>(it->second))
+            {
+                return std::get<File>(it->second).data.size();
+            }
+        }
+
+        auto [mnt, stem] = splitMount(pf);
+        if(!mnt.empty())
+        {
+            auto & M = std::get<Mount>(desc.at(mnt));
+            return std::filesystem::file_size(M.host / stem);
+        }
+        return 0;
+    }
+
     void print()
     {
         for(auto &[a,b] : desc)
@@ -312,319 +263,30 @@ struct VFS
         }
     }
 
+    host_path_type host_path(vfs_path_type const pf) const
+    {
+        assert(pf.is_absolute());
+
+        auto it = desc.find(pf);
+        // exists in the Virtual files
+        if(it != desc.end())
+        {
+            return {};
+            //if(std::holds_alternative<File>(it->second))
+            //{
+            //    auto & F = std::get<File>(it->second);
+            //    std::istringstream() F.data
+            //}
+        }
+        auto [mnt, stem] = splitMount(pf);
+        if(!mnt.empty())
+        {
+            auto & M = std::get<Mount>(desc.at(mnt));
+            return M.host / stem;
+        }
+        return {};
+    }
     std::map<vfs_path_type, FileDescriptor> desc;
-};
-
-struct HostPath
-{
-    std::vector<std::filesystem::path> pathOnHost;
-
-    HostPath() = default;
-
-    HostPath(std::filesystem::path const &p)
-    {
-        append(p);
-    }
-
-    HostPath(std::vector<std::filesystem::path> const &p)
-    {
-        for(auto & x : p)
-            append(x);
-    }
-
-    void append(std::filesystem::path const & p)
-    {
-        if( !std::filesystem::exists(p) || !std::filesystem::is_directory(p))
-        {
-            return;//throw std::runtime_error("The Host Path does not exist or is not a folder");
-        }
-        pathOnHost.push_back(p);
-    }
-
-
-    bool exists(std::filesystem::path relPath) const
-    {
-        return !host_path(relPath).empty();
-    }
-
-    std::filesystem::path host_path(std::filesystem::path relPath) const
-    {
-        for(auto it = pathOnHost.rbegin(); it!=pathOnHost.rend(); ++it)
-        {
-            auto p = *it / relPath.relative_path();
-            if(std::filesystem::exists(p))
-            {
-                return p;
-            }
-        }
-        return {};
-    }
-
-    template<typename callable_t>
-    void for_each(callable_t && c) const
-    {
-        for(auto it = pathOnHost.rbegin(); it!=pathOnHost.rend(); ++it)
-        {
-            auto & b = *it;
-            for(auto & A : std::filesystem::directory_iterator(b))
-            {
-                auto actualPath = A.path().lexically_relative(b);
-                c(actualPath);
-            }
-        }
-    }
-    template<typename callable_t>
-    void for_each(std::filesystem::path const & p, callable_t && c) const
-    {
-        for(auto it = pathOnHost.rbegin(); it!=pathOnHost.rend(); ++it)
-        {
-            auto & b = *it;
-            for(auto & A : std::filesystem::directory_iterator(b / p))
-            {
-                auto actualPath = A.path().lexically_relative(b);
-                c(actualPath);
-            }
-        }
-    }
-};
-
-struct VirtualFileSystem
-{
-    using vfs_path_type  = std::filesystem::path;
-    using host_path_type = std::filesystem::path;
-
-    VirtualFileSystem()
-    {
-        //mount("/", {});
-       // _rootMounts["/"] = {};
-    }
-    void mount(vfs_path_type mountPoint, HostPath p)
-    {
-        if(_rootMounts.count(mountPoint))
-            throw std::runtime_error("Mount point already exists");
-
-        if(_rootMounts.size() != 0 && !exists(mountPoint.parent_path()))
-            throw std::runtime_error("FOlder does not exist");
-
-        _rootMounts[mountPoint] = p;
-    }
-
-    void umount(vfs_path_type mountPoint)
-    {
-        _rootMounts.erase(mountPoint);
-    }
-
-    /**
-     * @brief exists
-     * @param vfsPath
-     * @return
-     *
-     * Returns true if the file exists in the VFS (and on the host)
-     */
-    bool exists(vfs_path_type const &vfsPath) const
-    {
-        auto strPath = vfsPath.generic_string();
-
-        for(auto & [a,b] : _rootMounts)
-        {
-            auto rootStr = a.generic_string();
-            if(rootStr == strPath)
-                return true;
-            auto n = strPath.find(rootStr);
-            if(n==0)
-            {
-                //fs::path("/resources/.bashrc").lexically_relative("/resources");
-                auto relPath = vfsPath.lexically_relative(a);
-                if(b.exists(relPath))
-                    return true;
-            }
-        }
-        return false;
-    }
-
-
-
-    template<typename callable_t>
-    void for_each(vfs_path_type const &vfsPath, callable_t && CC) const
-    {
-        auto a = vfsPath;
-        //while(!vfsPath.empty())
-        {
-            if(is_mount(a))
-            {
-                auto & M = _rootMounts.at(a);
-                if(M.pathOnHost.empty())
-                {
-                    // an empty mount
-                    for(auto & [mnt, M2] : _rootMounts)
-                    {
-                        if(mnt.parent_path() == vfsPath)
-                        {
-                            CC(mnt.filename());
-                        }
-                    }
-                }
-                M.for_each(CC);
-                return;
-            }
-            else
-            {
-                auto h = host_path(vfsPath);
-                for(auto & A : std::filesystem::directory_iterator(h))
-                {
-                    auto actualPath = A.path().filename();//lexically_relative(h);
-                    CC(actualPath);
-                }
-            }
-        }
-    }
-    template<typename callable_t>
-    void for_each1(vfs_path_type const &vfsPath, callable_t && CC) const
-    {
-        assert(vfsPath.is_absolute());
-        //std::cout << "Listing dir: " << vfsPath << std::endl;
-
-        if(is_mount(vfsPath))
-        {
-            // if this is a mount point
-            // Its possible that the
-
-            // eg: mnt = /A/B
-            //     vfsPath = /
-            // vfsPath is a base of mnt
-            for(auto & [mnt, M] : _rootMounts)
-            {
-                if(mnt == vfsPath) continue;
-                if(is_base_of(vfsPath, mnt))
-                {
-                    auto file = mnt.lexically_relative(vfsPath);
-                    if(!file.has_parent_path())
-                        CC(file);
-                }
-            }
-            auto & M  = _rootMounts.at(vfsPath);
-            M.for_each([&](auto const & file)
-            {
-                CC(file);
-            });
-        }
-        else
-        {
-            // not a mount point, so check all other mount points
-            for(auto & [mnt, M] : _rootMounts)
-            {
-                if(is_base_of(mnt, vfsPath ))
-                {
-
-                    if(M.exists(vfsPath.lexically_relative(mnt)))
-                    {
-                        //std::cout << mnt << "   " << std::endl;;
-                        //std::cout << "   " <<  vfsPath.lexically_relative(mnt) << std::endl;
-                        for(auto it = M.pathOnHost.rbegin(); it!=M.pathOnHost.rend(); ++it)
-                        {
-                            auto & b = *it;
-                            for(auto & A : std::filesystem::directory_iterator(b))
-                            {
-                                auto actualPath = A.path();
-                                CC(actualPath);
-                            }
-                        }
-                        //M.for_each([&CC](auto const & vv)
-                        //           {
-                        //               CC(vv);
-                        //           });
-                    }
-                }
-            }
-        }
-    }
-
-    void listDir(vfs_path_type const &vfsPath) const
-    {
-        std::cout << "Listing dir: " << vfsPath << std::endl;
-        for_each(vfsPath, [](auto const & rel)
-        {
-            std::cout << rel << std::endl;
-        });
-    }
-
-    bool is_mount(vfs_path_type const & vfsPath) const
-    {
-        for(auto & [a,b] : _rootMounts)
-        {
-            if(a == vfsPath)
-                return true;
-        }
-        return false;
-    }
-
-    bool is_directory(vfs_path_type const & vfsPath) const
-    {
-        if(_rootMounts.count(vfsPath))
-            return true;
-        return std::filesystem::is_directory(host_path(vfsPath));
-    }
-    bool is_regular_file(vfs_path_type const & vfsPath) const
-    {
-        if(_rootMounts.count(vfsPath))
-            return false;
-        return std::filesystem::is_regular_file(host_path(vfsPath));
-    }
-
-    /**
-     * @brief resolvedPath
-     * @param vfsPath
-     * @return
-     *
-     * Given a path in the VFS, return the actual path on the
-     * host file system
-     */
-    host_path_type host_path(vfs_path_type vfsPath) const
-    {
-        auto strPath = vfsPath.generic_string();
-
-        for(auto & [a,b] : _rootMounts)
-        {
-            auto rootStr = a.generic_string();
-            auto n = strPath.find(rootStr);
-
-            if(n==0)
-            {
-                //fs::path("/resources/.bashrc").lexically_relative("/resources");
-                auto relPath = vfsPath.lexically_relative(a);
-                auto real_path = b.host_path(relPath);
-                if(!real_path.empty())
-                    return real_path;
-            }
-        }
-        return {};
-    }
-
-    auto const& mount_points() const
-    {
-        return _rootMounts;
-    }
-
-
-protected:
-    std::map<vfs_path_type, HostPath> _rootMounts;
-};
-class ivfstream : public std::ifstream
-{
-public:
-    ivfstream(VirtualFileSystem * P, VirtualFileSystem::vfs_path_type path)
-        : std::ifstream(P->host_path(path), ios_base::in)
-    {
-    }
-};
-
-class ovfstream : public std::ofstream
-{
-public:
-    ovfstream(VirtualFileSystem * P, VirtualFileSystem::vfs_path_type path)
-        : std::ofstream(P->host_path(path), ios_base::in)
-    {
-    }
 };
 
 }
--- test/unit-VirtualFileSystem.cpp
+++ test/unit-VirtualFileSystem.cpp
@@ -11,7 +11,7 @@ void tree(gul::VFS const & fs, gul::VFS::vfs_path_type root = "/", std::string p
                     std::cout << prefix << v << std::endl;
                     if(fs.is_directory(root / v))
                     {
-                        tree(fs, root/v, prefix + "     ");
+                        tree(fs, root/v, prefix + "   ");
                     }
                 });
 }
@@ -36,138 +36,70 @@ SCENARIO("SDF2")
     gul::VFS fs;
 
     fs.mount("/src", std::filesystem::path(CMAKE_SOURCE_DIR));
+    fs.mount("/src2", std::filesystem::path(CMAKE_SOURCE_DIR));
+
+    {
+        auto [mount, stem] = fs.splitMount("/src/cmake/Coverage.cmake");
+        REQUIRE(mount == "/src");
+        REQUIRE(stem == "cmake/Coverage.cmake");
+    }
+    {
+        auto [mount, stem] = fs.splitMount("/src2/cmake/Coverage.cmake");
+        REQUIRE(mount == "/src2");
+        REQUIRE(stem == "cmake/Coverage.cmake");
+    }
+
+    REQUIRE(fs.exists("/"));
     REQUIRE(fs.exists("/src"));
+    REQUIRE(fs.exists("/src/test"));
     REQUIRE(fs.exists("/src/test/CMakeLists.txt")); // exists on filesystem
+    REQUIRE(fs.exists("/src/cmake"));
     REQUIRE(fs.exists("/src/cmake/Coverage.cmake"));
 
     REQUIRE(fs.is_directory("/"));
     REQUIRE(fs.is_directory("/src"));
     REQUIRE(fs.is_directory("/src/test"));
 
-    auto [mount, stem] = fs.splitMount("/src/cmake/Coverage.cmake");
-    REQUIRE(mount == "/src");
-    REQUIRE(stem == "cmake/Coverage.cmake");
-
     fs.mkdir("/build");
     fs.mkdir("/test");
     fs.mkdir("/test/hello/world");
 
+    fs.unmount("/src2");
+    REQUIRE(!fs.exists("/src2"));
+    REQUIRE(!fs.is_directory("/src2"));
+    REQUIRE(!fs.is_file("/src2"));
 }
 
-#if 0
-SCENARIO("SDF")
-{
 
-    namespace FS = std::filesystem;
-    REQUIRE(!gul::VirtualFileSystem::is_base_of("/root/A", "/root3/A/b/c/d"));
-    REQUIRE(gul::VirtualFileSystem::is_base_of("/root/A", "/root/A/b/c/d"));
-}
-
-void tree(gul::VirtualFileSystem const & fs, gul::VirtualFileSystem::vfs_path_type root = "/", std::string prefix="")
+SCENARIO("SDF3")
 {
-    //std::cout << root.filename() << std::endl;
-    fs.for_each(root, [&](auto const & v)
+    gul::VFS fs;
+
+    fs.mkdir("/res");
+    fs.mount("/res/src" , std::filesystem::path(CMAKE_SOURCE_DIR)/"cmake");
+    fs.mount("/res/src2", std::filesystem::path(CMAKE_SOURCE_DIR)/"test");
+
     {
-        std::cout << prefix << v << std::endl;
-        if(fs.is_directory(root / v))
-        {
-            tree(fs, root/v, prefix + "  ");
-        }
-    });
-}
-
-SCENARIO("test")
-{
-    gul::VirtualFileSystem fs;
-
-
-    namespace FS = std::filesystem;
-
-    FS::remove_all(FS::path(CMAKE_CURRENT_BINARY_DIR) / "mnt");
-    auto A = FS::path(CMAKE_CURRENT_BINARY_DIR) / "mnt" /"A";
-    auto B = FS::path(CMAKE_CURRENT_BINARY_DIR) / "mnt" /"B";
-
-    FS::create_directories( A / "1" / "a");
-    FS::create_directories( A / "2" / "b");
-    FS::create_directories( A / "3" / "c");
-    FS::create_directories( B / "4" / "d");
-    FS::create_directories( B / "5" / "e");
-    FS::create_directories( B / "6" / "f");
-
-    REQUIRE_NOTHROW(fs.mount("/", {}));
-    REQUIRE(fs.exists("/"));
-
-    REQUIRE_NOTHROW(fs.mount("/A", gul::HostPath{A}));
-
-
-    WHEN("We try to mount a folder thats already mounted we get an error")
+        auto [mount, stem] = fs.splitMount("/res/src/Coverage.cmake");
+        REQUIRE(mount == "/res/src");
+        REQUIRE(stem == "Coverage.cmake");
+    }
     {
-        REQUIRE_THROWS(fs.mount("/", gul::HostPath{B}));
+        auto [mount, stem] = fs.splitMount("/res/src2/CMakeLists.txt");
+        REQUIRE(mount == "/res/src2");
+        REQUIRE(stem == "CMakeLists.txt");
     }
 
-    fs.mount("/B", gul::HostPath{B});
-
-    REQUIRE( fs.exists("/B/4"));
-
-    fs.mount("/A/7", gul::HostPath{B});
-
-    fs.listDir("/");
-    fs.listDir("/A");
-    fs.listDir("/B");
-    fs.listDir("/B/4");
-
-}
-
-#if 0
-SCENARIO("Union")
-{
-    gul::VirtualFileSystem fs;
-
-    gul::HostPath H;
-    H.pathOnHost.push_back(std::filesystem::path(CMAKE_SOURCE_DIR) / "test");
-    H.pathOnHost.push_back(std::filesystem::path(CMAKE_SOURCE_DIR) / "cmake");
-    fs.mount("/", {});
-    fs.mount("/U", H);
-
-    //fs.print();
+    REQUIRE(fs.exists("/"));
+    REQUIRE(fs.exists("/res/src"));
+    REQUIRE(fs.exists("/res/src/Coverage.cmake"));
+    REQUIRE(fs.exists("/res/src2/CMakeLists.txt")); // exists on filesystem
 
     REQUIRE(fs.is_directory("/"));
-    REQUIRE(fs.is_directory("/U"));
+    REQUIRE(fs.is_directory("/res/src"));
+    REQUIRE(fs.is_directory("/res/src2"));
+    //REQUIRE(fs.is_directory("/res/src/test"));
 
-    REQUIRE(fs.is_mount("/"));
-    REQUIRE(fs.is_mount("/U"));
-
-    REQUIRE(fs.exists("/U/Coverage.cmake"));
-    REQUIRE(fs.exists("/U/unit-Transform.cpp"));
-
-
-    REQUIRE(fs.is_regular_file("/U/Coverage.cmake"));
-    REQUIRE(!fs.is_directory("/U/Coverate.cmake"));
-}
-
-
-SCENARIO("Union - SameFile")
-{
-    gul::VirtualFileSystem fs;
-
-    gul::HostPath H;
-
-    // lower directory
-    H.pathOnHost.push_back(std::filesystem::path(CMAKE_SOURCE_DIR));
-
-    // upper directory
-    H.pathOnHost.push_back(std::filesystem::path(CMAKE_SOURCE_DIR) / "test");
-
-    fs.mount("/", {});
-    fs.mount("/U", H);
-
-    //fs.print();
-
-    REQUIRE(fs.exists("/U/CMakeLists.txt"));
-    REQUIRE(fs.host_path("/U/CMakeLists.txt") == std::filesystem::path(CMAKE_SOURCE_DIR) / "test" / "CMakeLists.txt");
     tree(fs);
+
 }
-
-
-#endif
-#endif
