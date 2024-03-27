7301bc5a51fc29b0aed03156852fd3979cd58836
Gavin <gavin.n.lobo@gmail.com>
2024-03-27 08:40:51 -0400

    Added coroutine task

---
+++ include/gul/coroutine/task.h
@@ -0,0 +1,124 @@
+#ifndef GUL_COROUTINE_TASK_H
+#define GUL_COROUTINE_TASK_H
+
+#include <coroutine>
+
+namespace gul
+{
+
+template<typename T>
+struct promise_value
+{
+    T result;
+
+    void return_value(T && r )
+    {
+        result = std::move(r);
+    }
+};
+
+struct promise_void
+{
+    void return_void()
+    {
+
+    }
+};
+
+
+template<typename T>
+struct Task_t
+{
+    struct promise_type : public std::conditional_t< std::is_same_v<void,T>, promise_void, promise_value<T> >
+    {
+        bool _done = false;
+        // must have a default consturctor
+        promise_type() = default;
+
+        // this is the first method to get
+        // executed when a coroutine is
+        // called for the first time
+        // Its job is to construct the return
+        // object
+        Task_t<T> get_return_object()
+        {
+            //std::cout << "get_return_object()\n";
+            return Task_t<T>{std::coroutine_handle<promise_type>::from_promise(*this)};
+        }
+
+        // returns an awaiter. can return
+        // std::suspend_never/always
+        // This is called just before the corotuine
+        // starts execution. It specifies in what
+        // state the coroutine should start in.
+        // in this case we are not-suspending when
+        // we first start the coroutine
+        std::suspend_never initial_suspend() {
+            _done = false;
+            //std::cout << "initial suspend\n";
+            return {};
+        }
+
+        // executes when the coroutine finishes
+        // executing.
+        std::suspend_always final_suspend() noexcept {
+            _done = true;
+            //std::cout << "final suspend\n";
+            return {};
+        }
+
+        // if there are any exceptions thrown
+        // that are not handled, this function
+        // will be called
+        void unhandled_exception()
+        {
+            std::cout <<  "Unhandled Exception" << std::endl;
+        }
+
+        /**
+         * @brief return_void
+         *
+         * Called when co_return void; is called.
+         *
+         * This is called before final_suspend()
+         */
+        //void return_void()
+        //{
+        //    std::cout << "return_void()\n";
+        //}
+
+        //void return_value(T value) { result = std::move(value); }
+
+        //T result;
+    };
+
+
+    // a copy constructor that passes in a coroutine handle
+    // so that we can control the exceution
+    Task_t(std::coroutine_handle<promise_type> handle_) : handle(handle_)
+    {
+    }
+
+    void resume()
+    {
+        handle.resume();
+    }
+    T operator()()
+    {
+        //handle.resume();
+        return std::move(handle.promise().result);
+    }
+
+    bool done() const
+    {
+        return handle.done();
+    }
+
+    std::coroutine_handle<promise_type> handle;
+};
+
+template<typename T>
+using Task = Task_t<T>;
+
+}
+#endif
